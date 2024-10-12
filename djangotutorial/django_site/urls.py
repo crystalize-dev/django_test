1f4f91f1da99b89c7c3cb7da4fec30e35ea19dd8
crystalize-dev <igandreev@ptsecurity.com>
2024-10-12 16:06:05 +0300

    first commit

---
+++ djangotutorial/django_site/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+] + debug_toolbar_urls()
